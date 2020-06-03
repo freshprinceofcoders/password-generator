54c9bdade16e89ed9b851085280899151e3d0c54
Milan Solanki <66133186+freshprinceofcoders@users.noreply.github.com>
2020-06-03 13:18:14 +0100

    Add files via upload

--- password generator.py
+++ password generator.py
@@ -2,8 +2,11 @@ import random
 import code
 print()
 password = ""
-chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!"£$%^&*()_+-=<>.,/?@;:'
-
+chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+numbers = '0123456789'
+sym = "!£$%^&*()_+-=<>.,/?@;:"
+mix = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!£$%^&*()_+-=<>.,/?@;:'
+# print(random.choice(nums))
 
 num = input("Enter number of passwords?")
 num = int(num)
@@ -14,10 +17,40 @@ lens = int(lens)
 if (lens) < 6:
     exit()
 
+
+
+nums = input("Enter how many numbers you want in your password?")
+nums = int(nums)
+
+let = input("Enter how many numbers you want in your password?")
+let = int(nums)
+
+
+
+if (nums) > (lens):
+    print("")
+    exit()
+
+if (nums)+(let)> lens-1:
+    print("please make sure it sums up")
+    exit()
+
+
+
+
 print("\nYour passwords are here:")    
+
 
 for pwd in range(num):
     password = ''
-    for c in range(lens):
+  
+    
+
+    for l in range(let):
         password += random.choice(chars)
+    for i in range(nums):
+          password += random.choice(numbers)
+
+    for c in range(lens-(let+nums)):
+        password += random.choice(sym)
     print(password)
